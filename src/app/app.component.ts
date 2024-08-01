351f56f59aa2d2a512ca390aff4768a76b697885
Wojciech Zając <wojciechzajaaac@gmail.com>
2024-08-01 22:33:18 +0200

    feat: Implement drag and drop functionality to the list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent, TodoFormComponent, TodoListComponent } from './components';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HeaderComponent, TodoFormComponent, TodoListComponent]
+  imports: [HeaderComponent, TodoFormComponent, TodoListComponent, FooterComponent]
 })
 export class AppComponent {
 }
